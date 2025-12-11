515773ec376382283e6740ae840794fe9b87fc57
Mr-Jahanzaib-dev <jahanzaibkhalil48@gmail.com>
2025-12-12 01:43:54 +0500

    remove somthing

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Helmet } from 'react-helmet-async';
 import { Film, Play, Star, Info } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/Navbar';
